508234b42f0cfd9bed26ba1192ea45e2335f6dda
Miguel Aréjula Aisa <arejula10@gmail.com>
2023-12-11 12:35:53 +0100

    rehecho

--- app/src/main/java/es/unizar/eina/M46_comidas/ui/home.java
+++ app/src/main/java/es/unizar/eina/M46_comidas/ui/home.java
@@ -3,14 +3,23 @@ package es.unizar.eina.M46_comidas.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import es.unizar.eina.M46_comidas.R;
 
 public class home extends AppCompatActivity {
-
+      Button buttonPlatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        buttonPlatos = findViewById(R.id.buttonPlatos);
+
+        buttonPlatos.setOnClickListener(view -> {
+
+        });
+
+
     }
 }
