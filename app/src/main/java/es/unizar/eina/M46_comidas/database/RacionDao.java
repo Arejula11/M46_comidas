82341ea83af41420d4a9a5ad4fac2a1961b53e2e
Miguel Aréjula <arejula10@gmail.com>
2023-12-20 20:00:15 +0100

    racion dao

---
+++ app/src/main/java/es/unizar/eina/M46_comidas/database/RacionDao.java
@@ -0,0 +1,31 @@
+package es.unizar.eina.M46_comidas.database;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface RacionDao {
+    
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    long insert(Racion racion);
+
+    @Update
+    int update(Racion racion);
+
+    @Delete
+    int delete(Racion racion);
+
+    @Query("DELETE FROM racion")
+    void deleteAll();
+    
+    @Query("SELECT * FROM racion WHERE PedidoId = :pedidoId")
+    LiveData<List<Racion>> getRaciones(int pedidoId); //cambiar query
+    
+}
